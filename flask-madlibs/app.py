9aa311f75d422f3757f7eb074793681980c12dd7
Logan Winnie <loganwinnier@gmail.com>
2023-09-27 18:39:27 -0400

    add question form

--- flask-madlibs/app.py
+++ flask-madlibs/app.py
@@ -7,3 +7,13 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+
+@app.get("/questions")
+def display_questions():
+    """displays question form on page"""
+
+    return render_template(
+        "questions.html",
+        words=silly_story.prompts
+    )
